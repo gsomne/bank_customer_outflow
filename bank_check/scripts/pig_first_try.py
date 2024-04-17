96912419a22a60738e0c18afea65bae979f6f49e
pan-keke <dazerw@mail.ru>
2024-04-17 19:41:00 +0300

    its disgusting

--- bank_check/scripts/pig_first_try.py
+++ bank_check/scripts/pig_first_try.py
@@ -1,17 +1,13 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
-import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report,confusion_matrix
+from sklearn.metrics import accuracy_score
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.neural_network import MLPClassifier
 
 data = pd.read_csv('Churn_Modelling.csv')
 data = data.drop(['RowNumber', 'CustomerId', 'Surname'], axis=1)
-
 X = data.iloc[:,0:10].values
 y = data.iloc[:,10].values
 
@@ -21,27 +17,28 @@ X[:,1] = label_X_country_encoder.fit_transform(X[:,1])
 label_X_gender_encoder = LabelEncoder()
 X[:,2] = label_X_gender_encoder.fit_transform(X[:,2])
 
-transform = ColumnTransformer([("countries", OneHotEncoder(), [1])], remainder="passthrough") # 1 is the country column
+transform = ColumnTransformer([("countries", OneHotEncoder(), [1])], remainder="passthrough")
 X = transform.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
 
-print("Пассивно-агрессивный классификатор")
-pac = PassiveAggressiveClassifier(max_iter=100)
+sc=StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+
+print("Passive Aggressive Classifier")
+pac = PassiveAggressiveClassifier(random_state=35)
 pac.fit(X_train, y_train)
 y_pred = pac.predict(X_test)
 accuracy_ = accuracy_score(y_test, y_pred)
 print(f'Accuracy Score of Passive Aggresive Scassifier: {round(accuracy_*100,2)}%')
 
-mlp_clf = MLPClassifier(hidden_layer_sizes=(5,2),
-                        max_iter = 300,activation = 'relu',
-                        solver = 'adam')
-mlp_clf = MLPClassifier(hidden_layer_sizes=(150,100,50),
-                        max_iter = 300,activation = 'relu',
-                        solver = 'adam')
 
+print("MLP Classifier")
+mlp_clf = MLPClassifier(hidden_layer_sizes=(10,5), max_iter=300, activation='relu', solver='adam')
 mlp_clf.fit(X_train, y_train)
-ypred = mlp_clf.predict(X_test)
-
-accuracy = accuracy_score(y_test, ypred)
-print(f'Accuracy : {round(accuracy*100,2)}%')
+y_pred = mlp_clf.predict(X_test)
+accuracy_ = accuracy_score(y_test, y_pred)
+print(f'Accuracy Score of MLP Classifier: {round(accuracy_*100,2)}%')
